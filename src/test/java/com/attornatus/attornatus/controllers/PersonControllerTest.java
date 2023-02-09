00844dae1bb33adbc85ca813e2a232effdd72fe9
Marcos Vinicius Prysthon Nascimento <marcosvn@motorola.com>
2023-02-09 14:45:02 -0300

    Concluding integration tests

--- src/test/java/com/attornatus/attornatus/controllers/PersonControllerTest.java
+++ src/test/java/com/attornatus/attornatus/controllers/PersonControllerTest.java
@@ -20,6 +20,8 @@ import com.attornatus.attornatus.models.Address;
 
 import org.springframework.http.MediaType;
 import java.nio.charset.Charset;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -36,12 +38,52 @@ public class PersonControllerTest {
     public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8")); 
 
     @Test
-    void testCreate() {
+    void testCreate() throws Exception {
+        List<Person> people = this.setPeople();
 
+        // setting json body
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(people.get(0));
+
+        RequestBuilder postReq = MockMvcRequestBuilders.post("/pessoas")
+            .contentType(APPLICATION_JSON_UTF8)
+            .content(requestJson);
+
+        mock.perform(postReq).andExpect(status().isOk());
     }
 
     @Test
-    void testFindByNome() {
+    void testFindByNome() throws Exception {
+        String expected = "{\"id\":2,\"nome\":\"Victor\",\"dataDeNascimento\":\"22/09/1995\",\"enderecos\":[{\"id\":2,\"principal\":true,\"logradouro\":\"Rua Professor Augusto Lins e Silva\",\"cep\":\"51030-030\",\"numero\":\"80\",\"cidade\":\"Recife\"},{\"id\":3,\"principal\":false,\"logradouro\":\"Rua Pedro Paes Mendonca\",\"cep\":\"51111-202\",\"numero\":\"200\",\"cidade\":\"Recife\"}]}";
+
+        List<Person> people = this.setPeople();
+
+        // setting json body
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+
+        // posting all people to my H2 databse
+        String requestJson = ow.writeValueAsString(people.get(0));
+        RequestBuilder postReq = MockMvcRequestBuilders.post("/pessoas")
+            .contentType(APPLICATION_JSON_UTF8)
+            .content(requestJson);
+
+        mock.perform(postReq).andExpectAll(status().isOk());
+
+        requestJson = ow.writeValueAsString(people.get(1));
+        postReq = MockMvcRequestBuilders.post("/pessoas")
+            .contentType(APPLICATION_JSON_UTF8)
+            .content(requestJson);
+
+        mock.perform(postReq).andExpectAll(status().isOk());
+
+        // asserting
+        RequestBuilder request = MockMvcRequestBuilders.get("/pessoas/Victor");
+        MvcResult result = mock.perform(request).andReturn();
+        assertEquals(expected, result.getResponse().getContentAsString());
 
     }
 
@@ -49,27 +91,14 @@ public class PersonControllerTest {
     void testIndex() throws Exception {
         String expected = "[{\"id\":1,\"nome\":\"Marcos\",\"dataDeNascimento\":\"14/06/1999\",\"enderecos\":[{\"id\":1,\"principal\":true,\"logradouro\":\"Rua Ana Camelo da Silva\",\"cep\":\"51111-040\",\"numero\":\"256\",\"cidade\":\"Recife\"}]}]";
 
-        // setting addresses
-        Address add1 = new Address();
-        add1.setCep("51111-040");
-        add1.setCidade("Recife");
-        add1.setLogradouro("Rua Ana Camelo da Silva");
-        add1.setNumero("256");
-        add1.setPrincipal(true);
-
-        // setting people
-        Person marcos = new Person();
-        List<Address> marcosEnd = Arrays.asList(new Address[] {add1});
-
-        marcos.setDataDeNascimento("14/06/1999");
-        marcos.setEnderecos(marcosEnd);
-        marcos.setNome("Marcos");
+        // people[0] == marcos and people[1] == victor
+        List<Person> people = this.setPeople();
 
         // setting json body
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-        String requestJson = ow.writeValueAsString(marcos);
+        String requestJson = ow.writeValueAsString(people.get(0));
 
         RequestBuilder postReq = MockMvcRequestBuilders.post("/pessoas")
             .contentType(APPLICATION_JSON_UTF8)
@@ -81,5 +110,47 @@ public class PersonControllerTest {
         RequestBuilder request = MockMvcRequestBuilders.get("/pessoas");
         MvcResult result = mock.perform(request).andReturn();
         assertEquals(expected, result.getResponse().getContentAsString());
+    }
+
+    public List<Person> setPeople() {
+        // setting addresses
+        Address add1 = new Address();
+        add1.setCep("51111-040");
+        add1.setCidade("Recife");
+        add1.setLogradouro("Rua Ana Camelo da Silva");
+        add1.setNumero("256");
+        add1.setPrincipal(true);
+
+        Address add2 = new Address();
+        add2.setCep("51111-202");
+        add2.setCidade("Recife");
+        add2.setLogradouro("Rua Pedro Paes Mendonca");
+        add2.setNumero("200");
+        add2.setPrincipal(false);
+
+        Address add3 = new Address();
+        add3.setCep("51030-030");
+        add3.setCidade("Recife");
+        add3.setLogradouro("Rua Professor Augusto Lins e Silva");
+        add3.setNumero("80");
+        add3.setPrincipal(true);
+
+        // setting people
+        Person marcos = new Person();
+        List<Address> marcosEnd = Arrays.asList(new Address[] {add1});
+
+        marcos.setDataDeNascimento("14/06/1999");
+        marcos.setEnderecos(marcosEnd);
+        marcos.setNome("Marcos");
+
+        Person victor = new Person();
+        List<Address> victorAdd = Arrays.asList(new Address[] {add3, add2});
+
+        victor.setDataDeNascimento("22/09/1995");
+        victor.setEnderecos(victorAdd);
+        victor.setNome("Victor");
+
+        List<Person> returnList = Arrays.asList(new Person[] {marcos, victor});
+        return returnList;
     }
 }
